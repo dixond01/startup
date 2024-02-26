ea7f3fcc393873744ee8b0420540fee2e30b46ab
Darbi <darbiadixon@gmail.com>
2024-02-26 12:49:17 -0700

    Fix setStudyGroupName

--- startup/studygroup.js
+++ startup/studygroup.js
@@ -1,10 +1,10 @@
 function setStudyGroupName() {
     const studyNameVar = localStorage.getItem('studyName');
     const studyClassEls = document.getElementsByClassName('studyGroup');
-    for (el in studyClassEls) {
+    for (el of studyClassEls) {
         el.textContent = studyNameVar;
-}
-
+        console.log(el.textConent)
+    }
 };
 
 setStudyGroupName();
