1f3684537f421cabd8d359952d324a308816d9d4
Darbi <darbiadixon@gmail.com>
2024-02-26 11:44:01 -0700

    Update usersList

--- startup/login.js
+++ startup/login.js
@@ -5,20 +5,22 @@ function login() {
     localStorage.setItem('studyName', studyNameEl.value);
 
     if (localStorage.getItem('usersList')) {
-      let usersList = localStorage.getItem('usersList');
+      let usersList = JSON.parse(localStorage.getItem('usersList'));
     }
     else {
       let usersList = [];
     }
 //implement an array of objects with the keys 'name' and 'status' where 'status' is 'online' or 'offline'
     for (user in usersList) {
-      if (user.name === nameEl.value) {
-        user.status = online;
+      if (user['name'] === nameEl.value) {
+        user['status'] = "online";
       }
       else {
-        usersList.push({name: nameEl.value, statuus: online});
+        usersList.push({name: nameEl.value, status: "online"});
       }
     }
+    //updates usersList in localStorage (not sure if stringify in right place)
+    localStorage.setItem('usersList', JSON.stringify(usersList));
 
     window.location.href = 'discussion.html';
     //moving windows may not work. works in debugger but not otherwise
