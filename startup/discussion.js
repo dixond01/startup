4e3c2e194255774ab6f1b3c5a4f996cdea1cd31e
Darbi <darbiadixon@gmail.com>
2024-02-26 23:37:27 -0700

    Add date to localstorage, refresh discussion logic

--- startup/discussion.js
+++ startup/discussion.js
@@ -1,9 +1,4 @@
-if (localStorage.getItem('messageList')) {
-    window.messageList = JSON.parse(localStorage.getItem('messageList'));
-}
-else {
-    window.messageList = [];
-}
+
 
 function displayMessage(chat) {
     const discussionFeed = document.getElementById('discussionFeed');
@@ -52,14 +47,75 @@ function pushMessage() {
 function setDiscussion() {
     const currentDate = new Date();
     const currentDay = currentDate.getDate();
+    
     const currentMonth = currentDate.toLocaleString('default', { month: 'long' });
+    
 
+    if (localStorage.getItem('dateList')) {
+        window.dateList = JSON.parse(localStorage.getItem('dateList'));
+    }
+    else {
+        window.dateList = [];
+    }
+
+    function findTest(x) {
+        if (x.month === currentMonth) {
+            if (x.day === currentDay) {
+                return true;
+            }
+        }
+    }
+
+    // if (!dateList.find(x => {x.month === currentMonth && x.day === currentDay})) {
+    if (!dateList.find(findTest)) {
+        //archive discussion
+        //change to draw from database?
+        if (localStorage.getItem('date')) {
+            const archiveDate = JSON.parse(localStorage.getItem('date'));
+            const archiveMonth = archiveDate.month;
+            const archiveDay = archiveDate.day;
+            localStorage.setItem(`${archiveMonth}${archiveDay}Discussion`, JSON.stringify({month: archiveMonth, day: archiveDay, messages: messageList}));
+        }
+
+        //clear discussion
+        localStorage.removeItem('messageList');
+
+        //add date to dateList
+        dateList.push({month: currentMonth, day: currentDay});
+        localStorage.setItem('dateList', JSON.stringify(dateList));
+
+        
+        
+        
+    }
+    //update discussionName
     const discussionName = document.getElementById('discussionName');
     discussionName.appendChild(document.createTextNode(`${currentMonth} ${currentDay}`));
+
+    //update date in localstorage
+    localStorage.setItem('date', JSON.stringify({month: currentMonth, day: currentDay}));
+}
+
+
+//might break something
+if (localStorage.getItem('messageList')) {
+    window.messageList = JSON.parse(localStorage.getItem('messageList'));
+}
+else {
+    window.messageList = [];
+}
+
+setDiscussion();
+
+if (localStorage.getItem('messageList')) {
+    window.messageList = JSON.parse(localStorage.getItem('messageList'));
+}
+else {
+    window.messageList = [];
 }
 
 displayMessages();
-setDiscussion();
+
 
 //need to create and display the array 
 //need to add daily chat refresh with heading change
