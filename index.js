cd4c6e0526f30242b53ef4ac99c8432f8577c3ff
Darbi <darbiadixon@gmail.com>
2024-03-07 11:50:18 -0700

    Fix array of empty objects, add bodyParser middleware for strings, fix error message

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
+
+//middleware to parse the body?
+app.use(bodyParser.text());
 
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
--- public/createstudygroup.js
+++ public/createstudygroup.js
@@ -7,14 +7,14 @@ async function createStudyGroup() {
     const response = await fetch('/api/studygroups');
     studyGroups = await response.json();
 
-    if (studyName in studyGroups){ //NOT SURE IF VALID (what is the type of studyGroups?)
-        const errorEl = document.querySelector('#createStudyErrror');
-        errorEl.value = "That group name is taken. Please pick another." //is this how you change the value?? will it go away on refresh?
+    if (studyGroups.includes(studyName)){ //NOT SURE IF VALID (what is the type of studyGroups?)
+        const errorEl = document.getElementById('createStudyError');
+        errorEl.innerText = "That group name is taken. Please pick another." //is this how you change the value?? will it go away on refresh?
     }
     else {
-        const response = await fetch('/api/studygroup', { //FIX - returns an array of empty objects?
+        const response = await fetch('/api/studygroup', { 
             method: 'POST',
-            headers: {'content-type': 'application/x-www-form-urlencoded'},
+            headers: {'content-type': 'text/plain'},
             body: studyName, //removed JSON.stringify
          });
         const studyGroups = await response.json();
