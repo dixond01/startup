27ea14f3706d63ebcd8487b99c81e400ec5f41bc
Darbi <darbiadixon@gmail.com>
2024-04-08 13:06:30 -0600

    Add body to app comp

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body'>App will display here</div>;
+  return (
+    <div className='wrapper'>
+        <div className='headerAndMain'>
+            <header>
+                <h1 class="websiteTitle">InspireUs</h1>
+            </header>
+
+            <main>
+                App components go here
+            </main>
+        </div>
+        <footer>
+            <div class="container-fluid">
+            <span class="text-reset">Darbi Dixon</span>
+            <a class="text-reset" href="https://github.com/dixond01/startup">GitHub</a>
+            </div>
+        </footer> 
+    </div>
+    );
 }
