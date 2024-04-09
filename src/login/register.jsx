8d0b6cb976238c7c8dff5a5da22ebc9c036f0d3f
Darbi <darbiadixon@gmail.com>
2024-04-08 23:36:12 -0600

    Delete register.html

--- src/login/register.jsx
+++ src/login/register.jsx
@@ -59,29 +59,16 @@ export function Register() {
             <form className='form' onSubmit={handleSubmit}>
                 <label htmlFor="email">Email:</label>
                 <input htmlFor="email" id="email" placeholder="Your email here" name="email" value={email} onChange={handleChange}/>
-                {/* <div id="emailError" className="errorMessage"></div> */}
                 {loginError && <div id="emailError" className="errorMessage">{emailError}</div>}
                 <label htmlFor="name">Name:</label>
                 <input type="text" id="name" placeholder="Your name here" name="name" value={name} onChange={handleChange}/>
                 <label htmlFor="password">Password:</label>
                 <input type="password" id="password" placeholder="Your password here" name="password" value={password} onChange={handleChange}/>
-                {/* <div id="loginError" className="errorMessage"></div> */}
                 {loginError && <div id="loginError" className="errorMessage">{loginError}</div>}
                 <button className="btn btn-primary rounded-pill px-3" type="submit">Register</button>
                 
               </form>
               <div>Some random stuff</div>
-            {/* <div class="form">
-                <label for="email">Email:</label>
-                <input for="email" id="email" placeholder="Your email here"/>
-                <div id="emailError" class="errorMessage"></div>
-                <label for="name">Name:</label>
-                <input type="text" id="name" placeholder="Your name here" />
-                <label for="password">Password:</label>
-                <input type="password" id="password" placeholder="Your password here" />
-                <div id="loginError" class="errorMessage"></div>
-                <button class="btn btn-primary rounded-pill px-3" onclick="register()">Register</button>
-              </div> */}
     </main>
   );
 }
