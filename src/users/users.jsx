6f572496e8934182a18a0e3736c5194ded0b08f6
Darbi <darbiadixon@gmail.com>
2024-04-09 20:46:21 -0600

    Add users component functionality

--- src/users/users.jsx
+++ src/users/users.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './users.css';
 
-export function Users() {
+export  function Users() { //async?
+const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const response = await fetch('/api/users');
+            const usersList = await response.json();
+            setUsers(usersList);
+        };
+
+        fetchUsers();
+    }, []);
+
   return (
-    <main>
-      <div>users displayed here</div>
+    <main id='usersPage'>
+      <div id="imgTitle">
+          <div id="studyGroupTitle">
+              <h1 className="studyGroup">Users</h1>
+              <h3 id="members">Members: <span id="membersNumber">{users.length}</span></h3>
+          </div>
+      </div>
+      <hr />
+      <table id="users">
+            <tbody>
+                <tr>
+                  <th>User</th>
+                  <th>Status</th>
+                </tr>
+                {users.map(user => (
+                    <tr key={user.id}> 
+                        <td>{user.name}</td>
+                        <td>
+                            <span className={user.status}>●</span>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+      <br />
     </main>
   );
 }
